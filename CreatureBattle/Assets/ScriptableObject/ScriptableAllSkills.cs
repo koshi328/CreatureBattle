0742c068279172a0de64529ffc402e35bc6ad15a
koshi <koshi.rky@gmail.com>
2017-12-11 17:16:46 +0900

    エフェクトデータの追加、AllSkillDataの追加

---
+++ CreatureBattle/Assets/ScriptableObject/ScriptableAllSkills.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Example/Create AllSkills")]
+public class ScriptableAllSkills : ScriptableObject
+{
+    [System.Serializable]
+    public struct Skills
+    {
+        public string skillName;
+        public int id;
+        public Sprite sprite;
+    }
+
+    public Skills[] data;
+}
