7f4b5b3470c199e8dbf4fd2cdf89a120563b66e2
kasao <d837667@gmail.com>
2018-01-16 12:18:05 +0900

    ポーションの湧きを制限

--- CreatureBattle/Assets/Scripts/Item/ItemGenerator.cs
+++ CreatureBattle/Assets/Scripts/Item/ItemGenerator.cs
@@ -7,6 +7,7 @@ public class ItemGenerator : MonoBehaviour {
     [SerializeField]
     GameObject[] _portionPrefab;
 
+    List<GameObject> _portionInstance = new List<GameObject>();
     [SerializeField]
     float _generateInterval;
     [SerializeField]
@@ -24,18 +25,27 @@ public class ItemGenerator : MonoBehaviour {
 	void Update () {
         if (!PhotonNetwork.isMasterClient) return;
 
+        for (int i = 0; i < _portionInstance.Count; i++)
+        {
+            if (_portionInstance[i] == null)
+            {
+                _portionInstance.RemoveAt(i);
+            }
+        }
+
+        if (3 < _portionInstance.Count) return;
         _timer -= Time.deltaTime;
         if (_timer < 0.0f)
         {
             _timer = _generateInterval;
-            Vector3 pos = new Vector3(Random.Range(-9.0f, 20.0f), -0.35f, Random.Range(-12.4f, -2.5f));
+            Vector3 pos = new Vector3(Random.Range(-9.0f, 20.0f), -1.47f, Random.Range(-12.4f, -2.5f));
             if (Random.Range(0, 2) == 0)
             {
-                PhotonNetwork.InstantiateSceneObject("Items/LifePot_tex", pos, Quaternion.identity, 0, null);
+                _portionInstance.Add(PhotonNetwork.InstantiateSceneObject("Items/LifePot_tex", pos, Quaternion.identity, 0, null));
             }
             else
             {
-                PhotonNetwork.InstantiateSceneObject("Items/ManaPot_tex", pos, Quaternion.identity, 0, null);
+                _portionInstance.Add(PhotonNetwork.InstantiateSceneObject("Items/ManaPot_tex", pos, Quaternion.identity, 0, null));
             }
         }
     }
